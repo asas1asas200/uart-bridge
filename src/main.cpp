6ca77fa925ec786b2e938ffcbc1853a5340745fe
asas1asas200 <asas1asas200@gmail.com>
2024-12-09 18:18:17 +0800

    feat(mega): change to mega2560

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,48 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+	// delay(2000);
+	Serial.begin(9600);
+	Serial1.begin(9600);
+	Serial2.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+	// if (Serial.available() >= 20) {
+		// String data;
+		while (Serial.available()) {
+			// data += (char)Serial.read();
+			auto data = Serial.read();
+			Serial1.write(data);
+			Serial2.write(data);
+			delay(10);
+		}
+		// for (const auto &c : data) {
+		// 	Serial1.write(c);
+		// 	Serial2.write(c);
+		// 	delay(3);
+		// }
+	// }
+	delay(100);
+	if (Serial1.available()) {
+		while (Serial1.available()) {
+			Serial.write(Serial1.read());
+			delay(3);
+		}
+	} else if (Serial2.available()) {
+		while (Serial2.available()) {
+			Serial.write(Serial2.read());
+			delay(3);
+		}
+	}
+	while (Serial1.available()) {
+		Serial1.read();
+		delay(3);
+	}
+	while (Serial2.available()) {
+		Serial2.read();
+		delay(3);
+	}
+	delay(50);
 }
